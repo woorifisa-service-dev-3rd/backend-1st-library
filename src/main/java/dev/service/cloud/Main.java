7ad3530467529cb03141aadb0ff1a3b0125a0383
Kang Hyun Woo <gusdn741021@gmail.com>
2024-08-21 12:08:50 +0900

    refactor: main 원래 상태로 되돌리기

--- src/main/java/dev/service/cloud/Main.java
+++ src/main/java/dev/service/cloud/Main.java
@@ -1,6 +1,5 @@
 package dev.service.cloud;
 
-import dev.service.cloud.domain.card.CardConsole;
 import dev.service.cloud.global.DBUtil;
 import lombok.extern.slf4j.Slf4j;
 
@@ -9,5 +8,7 @@ import java.sql.Connection;
 @Slf4j
 public class Main {
     public static void main(String[] args) {
+        Connection connection = DBUtil.getConnection();
+        System.out.println(connection);
     }
 }
