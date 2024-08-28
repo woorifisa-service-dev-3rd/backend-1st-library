acfc18aac6b9daac22cebc9849dea5f1f669c1c8
Hyunwoo Kang <gusdn741021@gmail.com>
2024-08-28 09:38:00 +0900

    feat: DBUtil 생성

---
+++ servlet/semi-Library/src/main/java/global/DBUtil.java
@@ -0,0 +1,63 @@
+package global;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DBUtil {
+	public static Connection getConnection() {
+        Connection connection = null;
+        try {
+
+            String dbUrl = "jdbc:mysql://localhost:3306/testdb";
+            String userName = "root";
+            String password = "1234";
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // DB 커넥션 생성
+            connection = DriverManager.getConnection(dbUrl, userName, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } 
+        return connection;
+    }
+	
+	public static void close(Connection con, PreparedStatement pstmt, ResultSet rset) {
+		try {
+			if (rset != null) {
+				rset.close();
+				rset = null;
+			}
+			if (pstmt != null) {
+				pstmt.close();
+				pstmt = null;
+			}
+			if (con != null) {
+				con.close();
+				con = null;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void close(Connection con, PreparedStatement pstmt) {
+		try {
+
+			if (pstmt != null) {
+				pstmt.close();
+				pstmt = null;
+			}
+			if (con != null) {
+				con.close();
+				con = null;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
